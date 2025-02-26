ff42b285f32003b83bf1fc1945a0edfe62378ce0
Habbak Yasmine <yasmine.habbak@etu.univ-orleans.fr>
2025-02-26 13:29:25 +0100

    2

---
+++ mots_croise/assets/bootstrap.js
@@ -0,0 +1,6 @@
+import { Application } from "stimulus";
+import { definitionFor } from "stimulus/webpack-helpers";
+
+const app = new Application(document.documentElement);
+// Register any custom controllers here
+// app.register('some_controller_name', SomeImportedController);
