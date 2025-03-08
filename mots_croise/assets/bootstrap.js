a5504dc3b8c08f9286a4454c26a60a8616aa1868
Doha MAZLANI <doha.mazlani@etu.univ-orleans.com>
2025-03-08 23:59:57 +0100

    Création du projet

--- mots_croise/assets/bootstrap.js
+++
@@ -1,6 +0,0 @@
-import { Application } from "stimulus";
-import { definitionFor } from "stimulus/webpack-helpers";
-
-const app = new Application(document.documentElement);
-// Register any custom controllers here
-// app.register('some_controller_name', SomeImportedController);
